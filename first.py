cd87f96173a621d7d81fc98d9c88787e3fd14343
V1m4v0 <almax.crazy2015@yandex.ru>
2024-06-02 16:34:50 +0300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,24 @@
+a = "Длина слова"
+print("длина строки =", len(a))                         #вычисление количества символов в строке
+
+first = 10
+second = 15
+summa = first + second
+diff = first - second
+print("сумма чисел =", summa, "Разность чисел =", diff) #Вычисление суммы и разности
+
+first = 5
+second = 10
+thrid = 15
+mean = (first + second + thrid)/3
+print("Среднее арифметическое число =", int(mean))      #Вычисление среднего арифметического числа
+
+first_string = "понедельник"
+second_string = "вторник"
+print(first_string, ", ", second_string)                #Простые строчки
+
+a = 2
+b = 4
+c = 6
+f = (a*b) + (a*c)
+print((f**3)/2)                                         #Сложная формула
