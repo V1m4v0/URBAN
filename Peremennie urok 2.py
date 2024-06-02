89879013735a5f4e37e516bd7af8a8f7e55b49d8
V1m4v0 <almax.crazy2015@yandex.ru>
2024-06-02 20:03:12 +0300

    Delete Peremennie urok 2.py

--- Peremennie urok 2.py
+++
@@ -1,5 +0,0 @@
-dz = 12
-chasi = 1.5
-kurs = "Python"
-vremya = (chasi / dz)
-print("Курс:", kurs, ",", "всего задач:", dz, ",", "Затрачано часов:", chasi, ",", "Среднее время выполнения:", vremya, ".")
