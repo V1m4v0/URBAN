f6dbd280d4a62d95e4812bcccc3569c0b429cc0a
V1m4v0 <almax.crazy2015@yandex.ru>
2024-06-04 01:13:47 +0300

    Add files via upload
    Практическое задание по теме "Переменные."

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+dz = 12
+chasi = 1.5
+kurs = "Python"
+vremya = (chasi / dz)
+print("Курс:", kurs, ",", "всего задач:", dz, ",", "Затрачано часов:", chasi, ",", "Среднее время выполнения:", vremya, ".")
