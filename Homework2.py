7f5b2618c4c31675197c627aa8bd5f67697b2d22
V1m4v0 <almax.crazy2015@yandex.ru>
2024-06-04 01:17:48 +0300

    Add files via upload
    Практическое задание по теме "Переменные."

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+Hour_Homework = 12
+time = 1.5
+study = "Python"
+time_1 = (time / Hour_Homework)
+print("Курс:", study, ",", "всего задач:", Hour_Homework, ",", "Затрачано часов:", time, ",", "Среднее время выполнения:", time_1, ".")
